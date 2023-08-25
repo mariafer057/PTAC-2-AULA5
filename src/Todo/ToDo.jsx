45638485fade6c8af86de78f7e68341f8841b126
mariafer057 <mariafernandaalves057@gmail.com>
2023-08-25 13:19:20 -0400

    lkdxsajdsa

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function ToDo() {
+   const [atividade, setAtividade ] = useState("");
+   const [lista, setLista ] = useState([]);
    
     return (
         <div>
-            <h1>Lista de Atividades</h1>
             <Link to="/">home</Link>
-           
+            <h1>Lista de Atividades</h1>           
         </div>
     );
 }
