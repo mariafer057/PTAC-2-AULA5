4b7eb945c587c9b1a829129b51fb2219690d48e5
mariafer057 <mariafernandaalves057@gmail.com>
2023-09-05 09:58:29 -0400

    comitamento

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -1,32 +1,45 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./todo.css"
 
 export default function ToDo() {
-   const [atividade, setAtividade ] = useState("");
+   const [tipo, setTipo ] = useState("");
+   const [marca, setMarca ] = useState("");
+   const [preco, setPreco ] = useState("");
    const [lista, setLista ] = useState([]);
    const [id,setId] = useState(1);
     const salvar =(e) =>{
         e.preventDefault();
         setLista([...lista, {
-                atividade: atividade,
+                tipo: tipo, marca:marca, preco: preco,
+
                 id: id
         }]);
         setId(id + 1);
-        setAtividade("");
+        setTipo("");
+        setMarca("");
+        setPreco("");
     };
 
     return (
         <div>
             <Link to="/">home</Link>
-            <h1>Lista de Atividades</h1>    
+            <h2 class="txt">Shoes mult</h2>    
             <form onSubmit={salvar}>
-            <input value={atividade} type="text"
-            onChange={(e)=>{ setAtividade(e.target.value)}}/>
-            <button>ADD</button>   
+            <input value={tipo} type="text"
+            onChange={(e)=>{ setTipo(e.target.value)}}/>
+            <input value={marca} type="text"
+            onChange={(e)=>{ setMarca(e.target.value)}}/>
+            <input value={preco} type="text"
+            onChange={(e)=>{ setPreco(e.target.value)}}/>
+            <button class="btn btn-success">ADD</button>   
             </form>      
             {lista.map((ativ)=>
             <div key= {ativ.id}>
-                <p>{ativ.atividade}</p>
+
+                <p>Tipo: {ativ.tipo}</p>
+                <p>Marca: {ativ.marca}</p>
+                <p>Preco:R${ativ.preco}</p>
             </div>
             )} 
         </div>
