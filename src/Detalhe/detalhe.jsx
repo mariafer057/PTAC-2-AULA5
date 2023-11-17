6a548f4d20dc52ad19aa4f5d296adcb74b78c1bc
mariafer057 <mariafernandaalves057@gmail.com>
2023-11-17 14:24:13 -0400

    aula passada

--- src/Detalhe/detalhe.jsx
+++ src/Detalhe/detalhe.jsx
@@ -1,10 +1,18 @@
 import { useParams } from "react-router-dom";
+import Card from '../componentes/Card';
 
 export default function Detalhe(){
     const {id} = useParams();
+    const lista = JSON.parse(localStorage.getItem("Lista"));
+
+    const ativ = lista.filter((objeto) => {
+        if(objeto.id == id){
+            return objeto;
+        }
+        return null
+    })
+    console.log(ativ[0]);
     return(
-        <div>
-            <h1>Olá {id}</h1>
-        </div>
+       <Card ativ={ativ[0]}></Card>
     );
 }
---
+++ src/componentes/Card.jsx
@@ -0,0 +1,7 @@
+export default function Card((ativ)){
+    return(
+        <div>
+            <h1>{ativ.ativ}</h1>
+        </div>
+    );
+}
