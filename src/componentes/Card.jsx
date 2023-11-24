7ce37cfc76e1cf78098829d02fdc356f8f45640a
mariafer057 <131805494+mariafer057@users.noreply.github.com>
2023-11-24 09:31:50 -0400

    Finalmente

--- src/componentes/Card.jsx
+++ src/componentes/Card.jsx
@@ -1,7 +1,8 @@
-export default function Card(ativ){
+export default function Card({ativ}){
     return(
         <div>
             <h1>{ativ.ativ}</h1>
+            <h1>Olá Mafer Linda</h1>
         </div>
     );
 }
